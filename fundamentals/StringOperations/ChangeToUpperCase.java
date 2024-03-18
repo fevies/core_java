5d01de179f78b3495d0139b7c13d94a934c114d6
fevies <elviesodhiambo@gmail.com>
2024-03-18 14:12:22 +0300

    string to upper case and lowercase

---
+++ fundamentals/StringOperations/ChangeToUpperCase.java
@@ -0,0 +1,7 @@
+public class ChangeToUpperCase {
+	public static void main(String[] args) {
+
+		String changeToUpper = "Changes the folowing to upper case";
+		System.out.println("sentence = " + changeToUpper);
+	}
+}
